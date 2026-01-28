76e97105d11dca31fbf9541597d9d0f551a12a93
Saigeetha <saigeetha1316@gmail.com>
2026-01-28 19:29:33 +0530

    Fix API URL and rebuild frontend

--- student-app-frontend/src/app/services/student.service.ts
+++ student-app-frontend/src/app/services/student.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class StudentService {
-   private apiUrl = 'http://localhost:3000/students';
+   private apiUrl = 'https://student-backend-api-nwqi.onrender.com/students';
 
   constructor(private http: HttpClient) {}
 
